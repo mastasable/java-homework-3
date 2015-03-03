87d9e974493b3e27e5c1a082e805be8ab94fb9ea
Aleksandr Sobol <bonpet666@gmail.com>
2015-03-03 17:59:49 +0300

    procedure version is complete

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -5,15 +8,20 @@ import java.util.Stack;
  * Created by admin on 03.03.2015.
  */
 public class Calculator {
-    public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        Scanner scanner = new Scanner(System.in);
         Stack<Double> stack =  new Stack<Double>();
+        Map<String, Double> def = new HashMap<String, Double>();
 
         while (true){
-            String line = s.nextLine();
+            String line = scanner.nextLine();
             String []parts = line.split(" ");
             if (parts[0].equalsIgnoreCase("PUSH")){
-                stack.push(Double.parseDouble(parts[1]));
+                if(def.containsKey(parts[1])){
+                    stack.push(def.get(parts[1]));
+                } else {
+                    stack.push(Double.parseDouble(parts[1]));
+                }
             } else if (parts[0].equalsIgnoreCase("PRINT")){
                 System.out.println(stack);
             } else if (parts[0].equalsIgnoreCase("ADD")){
@@ -28,6 +36,9 @@ public class Calculator {
                 stack.push(stack.pop() - stack.pop());
             }  else if (parts[0].equalsIgnoreCase("QUIT")) {
                 break;
+            } else if (parts[0].equalsIgnoreCase("DEFINE")){
+                def.put(parts[1], new Double(Double.parseDouble(parts[2])));
+                System.out.println(parts[1] + " = " + def.get(parts[1]));
             }
         }
     }
